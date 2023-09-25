2e56e425b6c25498864449c22ec008b2b3e143bb
lilybw <lilybwanscher@gmail.com>
2023-09-25 11:25:00 +0200

    fetching data from the backend

--- car-rental/App.tsx
+++ car-rental/App.tsx
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { Car } from './src/ts/types';
 
 export default function App() {
-
   const [clickCount,setClickCount] = useState(0);
+  //show loading spinner on null
+  const [carData, setCarData] = useState<Car[] | null>(null);
+  const [networkError, setNetworkError] = useState<Error | null>(null);
+
+  React.useEffect(() => {
+    //Check local storage first, else:
+    fetch('https://localhost:3000/car-data')
+    .catch(err => {
+      console.log("Networking error: \n"+err);
+      setNetworkError(err);
+    }).then(res => res?.json())
+    .catch(err => console.log("Data formatting error: \n"+err))
+    .then(data => setCarData(data as Car[]));
+  })
+
 
   return (
     <View style={styles.container}>
---
+++ car-rental/src/ts/types.ts
@@ -0,0 +1,50 @@
+export type Car = {
+    model: string;
+    year: number;
+    weightKg: number;
+    dkkPrKm: number;
+    dimensions: number[];
+    lat: number;
+    lon: number; 
+    available: boolean;
+    id: number;
+}
+export type LicenseLocal = {
+    expirationDate: Date;
+    creationDate: Date;
+    surname: string;
+    name: string;
+    nationality: string;
+    socialSecurityNumber: string;
+    licenseId: string;
+    userId: number;
+    pictureUrl: string;
+}
+
+export type User = {
+    id: number;
+    licenseId: number;
+    username: string;
+    passwordHash: string;
+    consentsToToS: boolean;
+    recievesNewsletter: boolean;
+    homeAddr: string;
+}
+
+export type Invoice = {
+    orderNr: number;
+    carId: number;
+    userId: number;
+    kmTraveled: number;
+    destinationAddr: string;
+    originAddr: string;
+    date: Date;
+    total: number;
+    paymentMethod: "paypal" | "creditcard" | "mobilepay";
+    /** 
+     * For paypal: The mail connected to the paypal account.
+     * For creditcard: The last 4 digits of the credit card number.
+     * For mobilepay: The phone number connected to the mobilepay account.
+     */
+    methodIdentifier: string;
+}
