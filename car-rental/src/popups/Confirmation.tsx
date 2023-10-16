4d9e5311423cc16faa782de383b45f7147ae58e5
thoni21 <90039692+thoni21@users.noreply.github.com>
2023-10-16 16:37:48 +0200

    Base layout for confirmation view

--- car-rental/src/popups/Confirmation.tsx
+++ car-rental/src/popups/Confirmation.tsx
@@ -1,16 +1,90 @@
 import React, { useEffect } from 'react';
-import { DimensionValue, Image, Pressable, StyleSheet, SafeAreaView, Text, View, Keyboard } from 'react-native';
+import { DimensionValue, Image, Pressable, StyleSheet, SafeAreaView, Text, View, Keyboard, Modal, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StylingDefaults } from '../ts/styles';
+import { CarData } from '../ts/types';
+
 
 export interface ConfirmationProps {
     setPage: (view: JSX.Element) => void;
     setPopUp: (view: JSX.Element) => void;
+    car: CarData;
 }
 
-export default function Confirmation({setPage, setPopUp}: ConfirmationProps): JSX.Element {
+export default function Confirmation({setPage, setPopUp, car}: ConfirmationProps): JSX.Element {
     return (
         <View>
-            <Text>Confirmation</Text>
+            <Modal animationType="slide"  transparent={true}>
+                <LinearGradient colors={[StylingDefaults.colors.blueBase.hsl, StylingDefaults.colors.blueDark.hsl]} style={styles.modal}>   
+                    <View>
+                        {/* Car name */}
+                        <Text style={styles.modalHeader}>{car.manufacturer} {car.model}</Text>
 
+                        {/* Image of car */}
+
+                        {/* Confirmation */}
+                        <Text style={styles.modalHeader}>Purchase Complete!</Text>
+
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.modalText}>To Car</Text>
+                        </TouchableOpacity>  
+                    </View>
+                </LinearGradient>
+            </Modal>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modal: {
+        flexDirection :'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '60%',
+        height: '30%',
+        top: "40%",
+        alignSelf: "center"
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '50%',
+    },
+    closeButton: {
+        zIndex: 1,
+        position: "absolute",
+        top: "auto",
+        left: "auto",
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    modalText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    modalHeader: {
+        fontSize: 40,
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    button: {
+        margin: 'auto',
+            width: 200,
+            height: 40,
+            backgroundColor: 'rgb(70,88,129)',
+            borderRadius: 15,
+            marginBottom: 10,
+            padding: 8,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: 'auto',
+            marginLeft: 'auto',
+    }
+  });
